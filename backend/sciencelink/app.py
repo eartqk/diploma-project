7169f2bfcfce8f99a4e6f480c8acc9e420511c0a
Oleg Artamonov <eartqk@gmail.com>
2023-06-05 14:10:18 +0300

    Changed CORS and origins

--- backend/sciencelink/app.py
+++ backend/sciencelink/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+
 from fastapi.middleware.cors import CORSMiddleware
 
 from .api import router
@@ -10,14 +11,17 @@ app = FastAPI(
     version='1.0.0',
 )
 
-# origins = [
-#     "http://localhost:5173",
-#     "http://localhost:8080",
-# ]
+origins = [
+    "http://localhost:5173",
+    "http://localhost:8000",
+    "http://localhost",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:8000"
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
