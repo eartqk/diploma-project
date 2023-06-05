8dbbe239986b4ba82a3fa1b83cd0b160e981c714
Oleg Artamonov <eartqk@gmail.com>
2023-06-05 21:34:05 +0300

    Changed Editor for Posts

--- frontend/src/components/Editor.tsx
+++ frontend/src/components/Editor.tsx
@@ -1,10 +1,22 @@
 import { Textarea, Button, Stack } from "@mantine/core";
+import { useState } from "react";
+import sciLinkApi from "../store/sciLinkApi";
 
 const Editor = () => {
+  const [value, setValue] = useState("");
+  const [post] = sciLinkApi.useCreatePostMutation();
+
   return (
     <Stack align="flex-end">
-      <Textarea w="100%" autosize minRows={4} label="Новая запись" />
-      <Button>Опубликовать</Button>
+      <Textarea
+        value={value}
+        onChange={(event) => setValue(event.currentTarget.value)}
+        w="100%"
+        autosize
+        minRows={4}
+        label="Новая запись"
+      />
+      <Button onClick={() => post({ body: value })}>Опубликовать</Button>
     </Stack>
   );
 };
