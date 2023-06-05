dc962f3c70c21a7a64a497f5cf74c0c025a205e1
Oleg Artamonov <eartqk@gmail.com>
2023-06-05 21:27:02 +0300

    Changed Post component

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -8,9 +8,20 @@ import {
   Spoiler,
   Stack,
   Text,
+  Textarea,
+  Tooltip,
 } from "@mantine/core";
-import type { Post } from "../types/common";
-import { IconThumbUp } from "@tabler/icons-react";
+import { useState } from "react";
+import {
+  IconThumbUp,
+  IconTrash,
+  IconEdit,
+  IconCheck,
+  IconPencil,
+  IconX,
+} from "@tabler/icons-react";
+import sciLinkApi from "../store/sciLinkApi";
+import formatDate from "../utils/formatDate";
 
 const Post: React.FC<Post> = ({
   body,
@@ -18,30 +29,89 @@ const Post: React.FC<Post> = ({
   id,
   updated_at,
   user,
-  org,
+  organization,
   likes,
 }) => {
-  return (
-    <Card shadow="sm" key={id}>
-      <Stack>
-        <Group>
-          <Avatar src={user.avatar} />
+  const [deletePost] = sciLinkApi.useDeletePostMutation();
+  const [editPost] = sciLinkApi.useEditPostMutation();
+  const [isEditing, setEditing] = useState(false);
+  const [editText, setEditText] = useState("");
 
-          <Stack spacing={0}>
-            <Text>
-              {user.name} {user.surname}
-            </Text>
-            <Text color="dimmed" size="xs">
-              {created_at as string}
-            </Text>
-          </Stack>
+  return (
+    <Card withBorder shadow="xs" key={id}>
+      <Stack>
+        <Group position="apart">
+          <Group>
+            <Avatar
+              src={`${import.meta.env.VITE_MEDIA_BASE_URL}/${user.avatar_path}`}
+            />
+
+            <Stack spacing={0}>
+              <Text>
+                {user.name} {user.surname}
+              </Text>
+              <Group align="center">
+                <Text color="dimmed" size="xs">
+                  {formatDate(created_at)}
+                </Text>
+                {updated_at && (
+                  <Tooltip
+                    withArrow
+                    label={
+                      <Text color="dimmed" size="xs">
+                        {formatDate(updated_at)}
+                      </Text>
+                    }
+                  >
+                    <IconPencil color="gray" size="14px" />
+                  </Tooltip>
+                )}
+              </Group>
+            </Stack>
+          </Group>
+          <ActionIcon onClick={() => deletePost(id)}>
+            <IconTrash size="1.125rem" />
+          </ActionIcon>
         </Group>
-        <Divider />
-        <Spoiler maxHeight={250} showLabel="Подробнее" hideLabel="Скрыть">
-          <Text sx={{ whiteSpace: "pre-wrap" }}>{body}</Text>
-        </Spoiler>
+        {isEditing ? (
+          <Textarea
+            value={editText}
+            onChange={(event) => setEditText(event.currentTarget.value)}
+            autosize
+            minRows={4}
+          />
+        ) : (
+          <Spoiler maxHeight={250} showLabel="Подробнее" hideLabel="Скрыть">
+            <Text sx={{ whiteSpace: "pre-wrap" }}>{body}</Text>
+          </Spoiler>
+        )}
       </Stack>
-      <Group position="right" spacing={0}>
+      <Group position="right" spacing="xs">
+        {isEditing ? (
+          <Group spacing={0}>
+            <ActionIcon onClick={() => setEditing(false)}>
+              <IconX size="1.125rem" />
+            </ActionIcon>
+            <ActionIcon
+              onClick={() => {
+                editPost({ post_id: id, editText: { body: editText } })
+                  .unwrap()
+                  .then(() => setEditing(false));
+              }}
+            >
+              <IconCheck size="1.125rem" />
+            </ActionIcon>
+          </Group>
+        ) : (
+          <ActionIcon
+            onClick={() => {
+              setEditing(true);
+              setEditText(body);
+            }}
+          >
+            <IconEdit size="1.125rem" />
+          </ActionIcon>
+        )}
         <ActionIcon>
           <IconThumbUp size="1.125rem" />
         </ActionIcon>
