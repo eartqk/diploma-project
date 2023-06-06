4073b9c0e32b555d8122b1396190e7173a529a89
Oleg Artamonov <eartqk@gmail.com>
2023-06-06 11:56:07 +0300

    Models for subscriptions separated

--- backend/sciencelink/api/subscriptions.py
+++ backend/sciencelink/api/subscriptions.py
@@ -1,8 +1,7 @@
-from pydantic import BaseModel
-
 from fastapi import APIRouter, Depends, status
 
 from sciencelink.models.auth import UserAuthSchema
+from sciencelink.models.subscriptions import FollowOrganizationSchema, FollowUserResponseSchema
 from sciencelink.services.auth import get_current_user_from_cookies
 from sciencelink.services.subscriptions import SubscriptionsService
 
@@ -11,12 +10,6 @@ router = APIRouter(
     prefix='/follow',
     tags=['subscriptions'],
 )
-
-
-class FollowUserResponseSchema(BaseModel):
-    follower_user_id: int
-    followed_user_id: int
-    is_follow: bool
 
 
 @router.get('/user/{user_id}', response_model=FollowUserResponseSchema)
@@ -57,5 +50,47 @@ def unfollow_user(
     return FollowUserResponseSchema(
         follower_user_id=user.id,
         followed_user_id=user_id,
+        is_follow=False,
+    )
+
+
+@router.get('/organization/{org_id}', response_model=FollowOrganizationSchema)
+def check_follow_organization(
+        org_id: int,
+        user: UserAuthSchema = Depends(get_current_user_from_cookies),
+        service: SubscriptionsService = Depends(),
+):
+    is_follow = service.is_user_followed_org(follower_id=user.id, followed_org_id=org_id)
+    return FollowOrganizationSchema(
+        follower_user_id=user.id,
+        followed_org_id=org_id,
+        is_follow=is_follow,
+    )
+
+
+@router.post('/organization/{org_id}', status_code=status.HTTP_200_OK)
+def follow_organization(
+        org_id: int,
+        user: UserAuthSchema = Depends(get_current_user_from_cookies),
+        service: SubscriptionsService = Depends(),
+):
+    service.follow_org(follower_id=user.id, followed_org_id=org_id)
+    return FollowOrganizationSchema(
+        follower_user_id=user.id,
+        followed_org_id=org_id,
+        is_follow=True,
+    )
+
+
+@router.delete('/organization/{org_id}', status_code=status.HTTP_200_OK)
+def unfollow_organization(
+        org_id: int,
+        user: UserAuthSchema = Depends(get_current_user_from_cookies),
+        service: SubscriptionsService = Depends(),
+):
+    service.unfollow_org(follower_id=user.id, followed_org_id=org_id)
+    return FollowOrganizationSchema(
+        follower_user_id=user.id,
+        followed_org_id=org_id,
         is_follow=False,
     )
--- backend/sciencelink/services/subscriptions.py
+++ backend/sciencelink/services/subscriptions.py
@@ -1,6 +1,8 @@
 from fastapi import Depends
 
+from sciencelink.db import tables
 from sciencelink.db.session import Session, get_session
+from sciencelink.services.organizations import OrganizationsService
 from sciencelink.services.users import UsersService
 
 
@@ -9,39 +11,74 @@ class SubscriptionsService:
             self,
             session: Session = Depends(get_session),
             users_service: UsersService = Depends(),
+            orgs_service: OrganizationsService = Depends(),
     ):
         self.session = session
         self.users_service = users_service
+        self.orgs_service = orgs_service
 
-    def _get_follower_and_followed_user(self, follower_id: int, followed_id: int):  # TODO: Optimize queries
+    def _get_follower_and_followed_user(
+            self, follower_id: int, followed_id: int
+    ) -> (tables.User, tables.User):
         follower_user = self.users_service.get(follower_id)
         followed_user = self.users_service.get(followed_id)
         return follower_user, followed_user
 
-    # def _get_follower_and_followed_org(self, follower_id: int, followed_org_id: int):
-    #     follower_user = self.users_service.get(follower_id)
-    #     followed_org = self.users_service.get(followed_id)
-    #     return follower_user, followed_user
+    def _get_follower_and_followed_org(
+            self, follower_id: int, followed_org_id: int
+    ) -> (tables.User, tables.Organization):
+        follower_user = self.users_service.get(follower_id)
+        followed_org = self.orgs_service.get(followed_org_id)
+        return follower_user, followed_org
+
+    def _is_following_user(self, follower_id: int, followed_id: int) -> bool:
+        return (
+            self.session.query(tables.follower_user)
+            .filter_by(
+                follower_user_id=follower_id,
+                followed_user_id=followed_id,
+            )
+            .count() > 0
+        )
+
+    def _is_following_org(self, follower_id: int, followed_org_id: int) -> bool:
+        return (
+            self.session.query(tables.follower_organization)
+            .filter_by(
+                follower_user_id=follower_id,
+                followed_org_id=followed_org_id,
+            )
+            .count() > 0
+        )
 
     def is_user_followed_user(self, follower_id: int, followed_id: int) -> bool:
         follower_user, followed_user = self._get_follower_and_followed_user(follower_id, followed_id)
-        return follower_user.is_following_user(followed_user)
+        return self._is_following_user(follower_user.id, followed_user.id)
 
     def follow_user(self, follower_id: int, followed_id: int):
         follower_user, followed_user = self._get_follower_and_followed_user(follower_id, followed_id)
-        follower_user.follow_user(followed_user)
-        self.session.commit()
+        if not self._is_following_user(follower_user.id, followed_user.id):
+            follower_user.followed_users.append(followed_user)
+            self.session.commit()
 
     def unfollow_user(self, follower_id: int, followed_id: int):
         follower_user, followed_user = self._get_follower_and_followed_user(follower_id, followed_id)
-        follower_user.unfollow_user(followed_user)
-        self.session.commit()
+        if self._is_following_user(follower_user.id, followed_user.id):
+            follower_user.followed_users.remove(followed_user)
+            self.session.commit()
 
-    def is_user_followed_org(self) -> bool:
-        pass
+    def is_user_followed_org(self, follower_id: int, followed_org_id: int) -> bool:
+        follower_user, followed_org = self._get_follower_and_followed_org(follower_id, followed_org_id)
+        return self._is_following_org(follower_user.id, followed_org.id)
 
-    def follow_org(self):
-        pass
+    def follow_org(self, follower_id: int, followed_org_id: int):
+        follower_user, followed_org = self._get_follower_and_followed_org(follower_id, followed_org_id)
+        if not self._is_following_org(follower_user.id, followed_org.id):
+            follower_user.followed_orgs.append(followed_org)
+            self.session.commit()
 
-    def unfollow_org(self):
-        pass
+    def unfollow_org(self, follower_id: int, followed_org_id: int):
+        follower_user, followed_org = self._get_follower_and_followed_org(follower_id, followed_org_id)
+        if self._is_following_org(follower_user.id, followed_org.id):
+            follower_user.followed_orgs.remove(followed_org)
+            self.session.commit()
