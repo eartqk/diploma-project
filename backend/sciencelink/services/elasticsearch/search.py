668d07aabde8a88b3d3b47e7ec3a8a4ec83a2bd5
Oleg Artamonov <eartqk@gmail.com>
2023-06-07 00:18:36 +0300

    Added ElasticSearch service

---
+++ backend/sciencelink/services/elasticsearch/search.py
@@ -0,0 +1,49 @@
+from elasticsearch import Elasticsearch
+
+from fastapi import HTTPException
+
+
+class SearchService:
+    def __init__(self):
+        self.es = Elasticsearch()
+
+    def index_post(self, index_name, post_id, user_id, body):
+        post_data = {
+            'post_id': post_id,
+            'user_id': user_id,
+            'body': body
+        }
+
+        try:
+            response = self.es.index(index=index_name, id=post_id, body=post_data)
+            return response
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Error indexing post: {str(e)}")
+
+    def update_post(self, index_name, post_id, body):
+        update_data = {
+            'doc': {
+                'body': body
+            }
+        }
+
+        try:
+            response = self.es.update(index=index_name, id=post_id, body=update_data)
+            return response
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Error updating post: {str(e)}")
+
+    def search_posts(self, index_name, query):
+        search_body = {
+            'query': {
+                'match': {
+                    'body': query
+                }
+            }
+        }
+
+        try:
+            response = self.es.search(index=index_name, body=search_body)
+            return response['hits']['hits']
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Error searching posts: {str(e)}")
