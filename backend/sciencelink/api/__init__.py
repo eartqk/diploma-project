0e6cd72a85c7986abdfdc835c03ad211cc2e7d07
Oleg Artamonov <eartqk@gmail.com>
2023-06-07 00:20:30 +0300

    Added comments in router api

--- backend/sciencelink/api/__init__.py
+++ backend/sciencelink/api/__init__.py
@@ -2,12 +2,13 @@ from fastapi import APIRouter
 
 from . import (
     auth,
-    institutes,
-    countries,
     users,
     organizations,
     subscriptions,
     posts,
+    comments,
+    institutes,
+    countries,
 )
 
 
@@ -17,5 +18,6 @@ router.include_router(users.router)
 router.include_router(organizations.router)
 router.include_router(subscriptions.router)
 router.include_router(posts.router)
+router.include_router(comments.router)
 router.include_router(institutes.router)
 router.include_router(countries.router)
